4ffc1f0dac26a34e815cec7440bd198cab55c4fb
Leticia Tambelli <leticiatambelli2@gmail.com>
2025-07-02 11:10:26 -0300

    print sem a casa decimal

--- src/ConversaoTemperatura.java
+++ src/ConversaoTemperatura.java
@@ -3,6 +3,11 @@ public class ConversaoTemperatura {
         double temperaturaCelcius = 30.5;
         double temperaturaFarenheit = (temperaturaCelcius * 1.8) + 32;
 
+        int temperaturaCelciusInteira = (int) temperaturaCelcius;
+        int temperaturaFarenheitInteira = (int) temperaturaFarenheit;
+
+
         System.out.println("A temperatura " + temperaturaCelcius + "ºC convertida em Farenheit é " + temperaturaFarenheit + "ºF.");
+        System.out.println("Essas temperaturas sem a casa decimal são: " + temperaturaCelciusInteira + "ºC e " + temperaturaFarenheitInteira + "ºF.");
     }
 }
