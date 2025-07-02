f3d6cbe1a88bb1decbe10ef1da9ab96d7ae6c74e
Leticia Tambelli <leticiatambelli2@gmail.com>
2025-07-02 10:58:06 -0300

    primeiro commit

---
+++ src/ConversaoTemperatura.java
@@ -0,0 +1,8 @@
+public class ConversaoTemperatura {
+    public static void main(String[] args) {
+        double temperaturaCelcius = 30.5;
+        double temperaturaFarenheit = (temperaturaCelcius * 1.8) + 32;
+
+        System.out.println("A temperatura " + temperaturaCelcius + "ºC convertida em Farenheit é " + temperaturaFarenheit + "ºF.");
+    }
+}
